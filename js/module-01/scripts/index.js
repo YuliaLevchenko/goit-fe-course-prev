01c4b0c938c41103b852b1b3c931dd326f87590b
Yulia <levchenkoITstart@gmail.com>
2018-12-01 22:44:40 +0200

    add module-01

---
+++ js/module-01/scripts/index.js
@@ -0,0 +1,21 @@
+const adminLogin = 'admin';
+
+let loginInput = prompt ('Введите логин');
+
+if (!loginInput) {
+    alert ('Отменено пользователем!');
+} else if (loginInput === adminLogin) {
+    const adminPassword = 'm4ng0h4ckz';
+
+    let passInput = prompt ('Введите пароль');
+
+    if (!passInput) {
+        alert ('Отменено пользователем!');
+    } else if (passInput === adminPassword) {
+        alert ('Добро пожаловать!');
+    } else {
+        alert ('Доступ запрещен, неверный пароль!');
+    }
+} else {
+    alert ('Доступ запрещен, неверный логин!');
+}
